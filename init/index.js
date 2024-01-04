41502a98ece75be656878db1d8f71ea29d0950aa
anisha-kumari001 <155528256+anisha-kumari001@users.noreply.github.com>
2024-01-04 11:20:25 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://2022ugcs001:anisha9279@cluster0.rwutq0a.mongodb.net/";
 
 main()
   .then(() => {
